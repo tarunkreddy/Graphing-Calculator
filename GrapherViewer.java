d34c37b60301971ce8fe02dfdbf51d9b11703476
Tarun Reddy <tarunkreddy@gmail.com>
2014-08-06 15:50:26 -0700

    Create GrapherViewer.java

---
+++ GrapherViewer.java
@@ -0,0 +1,12 @@
+package Tests;
+import javax.swing.JFrame;
+public class GrapherViewer {
+
+	public static void main(String[] args) {
+		JFrame frame = new GrapherFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+
+	}
+
+}
