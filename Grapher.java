df60747d04bec0f226b1fe7424703a8647efa726
Tarun Reddy <tarunkreddy@gmail.com>
2014-08-06 15:45:10 -0700

    Create Grapher.java

---
+++ Grapher.java
@@ -0,0 +1,16 @@
+package Tests;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Grapher extends JFrame{
+	
+	public Grapher() {
+	}
+	
+	
+
+}
